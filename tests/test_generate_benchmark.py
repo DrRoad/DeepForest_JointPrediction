44cb3ed5eadb1fa15e7f8377079913fc9180b324
bw4sz <benweinstein2010@gmail.com>
2020-06-02 19:01:48 -0700

    passes locally again

---
+++ tests/test_generate_benchmark.py
@@ -0,0 +1,18 @@
+#test submission document
+import os
+import sys
+sys.path.append(os.path.dirname(os.getcwd()))
+import pandas as pd
+
+from DeepForest_JointPrediction.data import generate_benchmark
+
+
+def test_generate_benchmark():
+    generate_benchmark.run(rgb_dir="data/benchmark/RGB/", savedir="output/")
+
+    fname = os.path.join("output/benchmark_annotations.csv")
+    assert os.path.exists(fname)
+
+    boxes = pd.read_csv(fname)
+    assert not boxes.empty
+    assert boxes.shape[1] == 6
